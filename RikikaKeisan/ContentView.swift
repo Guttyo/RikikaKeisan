81b02990887a1cb1cd57672222ec68b9bb17f616
Guttyo <takafumi.mizuguchi@gmail.com>
2021-09-16 10:08:56 +0900

    Initial Commit

---
+++ RikikaKeisan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RikikaKeisan
+//
+//  Created by 水口貴史 on 2021/09/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RikikaKeisan/RikikaKeisanApp.swift
@@ -0,0 +1,17 @@
+//
+//  RikikaKeisanApp.swift
+//  RikikaKeisan
+//
+//  Created by 水口貴史 on 2021/09/16.
+//
+
+import SwiftUI
+
+@main
+struct RikikaKeisanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
