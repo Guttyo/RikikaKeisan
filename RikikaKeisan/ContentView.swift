3bf3bd2a851ae6f952f2d01a4be26198a85e02e2
Guttyo <takafumi.mizuguchi@gmail.com>
2021-09-17 17:45:15 +0900

    ボタン追加

--- RikikaKeisan/ContentView.swift
+++ RikikaKeisan/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack{
+        Text("力価計算アプリ")
             .padding()
+        Text("注意書き")
+            .padding()
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+            })
+        }
     }
 }
 
--- RikikaKeisan/RikikaKeisanApp.swift
+++ RikikaKeisan/RikikaKeisanApp.swift
@@ -1,7 +1,7 @@
 //
 //  RikikaKeisanApp.swift
 //  RikikaKeisan
-//
+//　力価を計算するサンプルアプリ
 //  Created by 水口貴史 on 2021/09/16.
 //
 
